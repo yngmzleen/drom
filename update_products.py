90c16104f1046f18a82fbce9120cb75c13b5eccf
yngmzleen <160007357+yngmzleen@users.noreply.github.com>
2024-07-22 13:41:39 +0700

    Update update_products.py

--- update_products.py
+++ update_products.py
@@ -5,46 +5,40 @@ import xml.etree.ElementTree as ET
 
 # Настройки для API
 url = "http://ka2.sibzapaska.ru/API/hs/v1/product"
-username = os.getenv("API_USERNAME", "API_client")
-password = os.getenv("API_PASSWORD", "rWp7mFWXRKOq")
+username = "API_client"
+password = "rWp7mFWXRKOq"
 
 # Создание заголовка для Basic Auth
 auth = base64.b64encode(f"{username}:{password}".encode()).decode()
 
-try:
-    print("Отправка запроса к API...")
-    headers = {"Authorization": f"Basic {auth}"}
-    response = requests.get(url, headers=headers)
-    
-    # Для отладки: выводим заголовки запроса и ответа
-    print(f"Заголовки запроса: {response.request.headers}")
-    print(f"Заголовки ответа: {response.headers}")
-    
-    response.raise_for_status()  # Проверка на наличие ошибок HTTP
+# Выполнение запроса к API
+response = requests.get(url, headers={"Authorization": f"Basic {auth}"})
+response.raise_for_status()  # Проверка на наличие ошибок
 
-    # Обработка ответа в формате JSON
-    data = response.json()
-    print(f"Получены данные: {data}")
+# Обработка ответа в формате JSON
+data = response.json()
 
-    # Создание корневого элемента XML
-    root = ET.Element("Products")
+# Создание корневого элемента XML
+root = ET.Element("Products")
 
-    # Преобразование каждого товара в XML элемент
-    for item in data:
-        product = ET.SubElement(root, "Product")
-        for key, value in item.items():
-            if key != "Оптовая_Цена":  # Исключаем поле "Оптовая_Цена"
-                element = ET.SubElement(product, key)
-                element.text = str(value)
-    print("XML структура создана.")
+# Преобразование каждого товара в XML элемент
+for item in data:
+    product = ET.SubElement(root, "Product")
+    for key, value in item.items():
+        if key != "Оптовая_Цена":  # Исключаем поле "Оптовая_Цена"
+            element = ET.SubElement(product, key)
+            element.text = str(value)
 
-    # Создание дерева XML и запись в файл
-    tree = ET.ElementTree(root)
-    with open("products.xml", "wb") as file:
-        tree.write(file, encoding="utf-8", xml_declaration=True)
-    print("XML файл успешно создан.")
-except requests.exceptions.HTTPError as http_err:
-    print(f"HTTP ошибка: {http_err}")
-except Exception as e:
-    print(f"Произошла ошибка: {e}")
-    raise
+    # Определение тега для товара
+    nomenclature = item.get("Номенклатура", "")
+    if re.match(r'^(1[2-9]|2[0-4])\s', nomenclature):
+        product.tag = "disk"
+    else:
+        product.tag = "tyres"
+
+# Создание дерева XML и запись в файл
+tree = ET.ElementTree(root)
+with open("products.xml", "wb") as file:
+    tree.write(file, encoding="utf-8", xml_declaration=True)
+
+print("XML файл успешно создан.")
